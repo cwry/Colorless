871f5e55354ee4a2c09eecea3879b443ddca9027
cwry <daniel.pochert@gmail.com>
2016-11-22 10:20:30 +0100

    path movement, color trigger

---
+++ Assets/Scripts/Level Editor/Visual Scripting/FollowITweenPath.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FollowITweenPath : MonoBehaviour {
+    public void move(GameObject go, iTweenPath path, float speed, bool reverse, Action done) {
+        Vector3[] nodes = path.nodes.ToArray();
+        if (reverse) Array.Reverse(nodes);
+        iTween.MoveTo(go, iTween.Hash("path", nodes, "speed", speed, "easeType", "easeInOutSine", "onComplete", (Action<object>)((arg) => { if (done != null) done(); })));
+    }
+}
---
+++ Assets/Scripts/Level Editor/Visual Scripting/Triggers/ColorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using uFAction;
+
+public class ColorTrigger : MonoBehaviour {
+    public GameObject distanceCheckTarget;
+    public float maxDistance = 1;
+    [SerializeField] private bool once = false;
+    private bool fired = false;
+    [ShowDelegate][SerializeField] private KickassDelegate onTrigger;
+
+    void Update() {
+        if (!(!once || !fired)) return;
+        if (Input.GetKeyDown(KeyCode.C)) {
+            if(maxDistance >= 0) {
+                Vector2 delta = new Vector2(transform.position.x - distanceCheckTarget.transform.position.x, transform.position.y - distanceCheckTarget.transform.position.y);
+                float distance = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y);
+                if (distance > maxDistance) return;
+            }
+            fired = true;
+            onTrigger.InvokeWithEditorArgs();
+        }
+    }
+}
