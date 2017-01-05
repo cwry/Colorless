27c448dd3da09b61b328a4b3cc78106728f98d53
valentin bach <valentin691@hotmail.de>
2017-01-05 19:52:56 +0100

    Tasks
    Nicht Vollständiges Circle Menu (Nur mit Maus und keine Farben)
    -Simples IngameMenu
    -Fixed MainMenu problems

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Transform pauseMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+	}
+
+    public void Pause() {
+
+        if (pauseMenuCanvas.gameObject.activeInHierarchy == false) {
+            pauseMenuCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenuCanvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Exit() {
+        pauseMenuCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        Application.LoadLevel("MainMenu");
+    }
+}
