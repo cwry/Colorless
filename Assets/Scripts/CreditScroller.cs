47aa86ecff317a1cd868bab9b64bea765d387309
cwry <daniel.pochert@gmail.com>
2017-02-01 07:45:44 +0100

    block movement on color animation, credits scroller scene, individual tweens

---
+++ Assets/Scripts/CreditScroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditScroller : MonoBehaviour {
+
+    public float speed;
+    public float endPosition;
+    public Text txt;
+
+    void Update() {
+        txt.rectTransform.position = new Vector3(txt.rectTransform.position.x, txt.rectTransform.position.y + Time.deltaTime * speed, txt.rectTransform.position.z);
+        if(txt.rectTransform.anchoredPosition.y >= endPosition) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- Assets/Scripts/Level Editor/Visual Scripting/Tween.cs
+++ Assets/Scripts/Level Editor/Visual Scripting/Tween.cs
@@ -7,6 +7,17 @@ public class Tween : MonoBehaviour {
         iTween.RotateTo(target, iTween.Hash("rotation", goal.transform.eulerAngles, "time", time, "easeType", "easeInOutSine"));
         iTween.MoveTo(target, iTween.Hash("position", goal.transform.position, "time", time, "easeType", "easeInOutSine", "onComplete", (Action<object>)((arg) => { if (done != null) done.curryTrigger()(); })));
         iTween.ScaleTo(target, iTween.Hash("scale", goal.transform.localScale, "time", time, "easeType", "easeInOutSine"));
+    }
 
+    public void tweenScale(GameObject target, Transform goal, float time, PristineTrigger done) {
+        iTween.ScaleTo(target, iTween.Hash("scale", goal.transform.localScale, "time", time, "easeType", "easeInOutSine", "onComplete", (Action<object>)((arg) => { if (done != null) done.curryTrigger()();})));
+    }
+
+    public void tweenPosition(GameObject target, Transform goal, float time, PristineTrigger done) {
+        iTween.MoveTo(target, iTween.Hash("position", goal.transform.position, "time", time, "easeType", "easeInOutSine", "onComplete", (Action<object>)((arg) => { if (done != null) done.curryTrigger()();})));
+    }
+
+    public void tweenRotation(GameObject target, Transform goal, float time, PristineTrigger done) {
+        iTween.RotateTo(target, iTween.Hash("rotation", goal.transform.eulerAngles, "time", time, "easeType", "easeInOutSine", "onComplete", (Action<object>)((arg) => { if (done != null) done.curryTrigger()(); })));
     }
 }
