6c6613cd2262d91f244d26f64cf01e836eacb875
Erick090 <ferick@hotmail.de>
2017-02-02 11:53:00 +0100

    Tweens at end of level

---
+++ Assets/Scripts/Level Editor/Visual Scripting/FlipSkeleton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+public class FlipSkeleton : MonoBehaviour {
+    public void flipX(SkeletonAnimation sa, bool flip){
+        sa.skeleton.flipX = flip;
+    }
+
+    public void flipY(SkeletonAnimation sa, bool flip){
+        sa.skeleton.flipY = flip;
+    }
+}
