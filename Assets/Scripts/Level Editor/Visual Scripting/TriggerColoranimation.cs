1ead4ed6d859603898d2d1267f1b7f4af0b3a8a6
cwry <daniel.pochert@gmail.com>
2017-02-06 12:04:40 +0100

    walk only - color animation vshelper - footsteps

---
+++ Assets/Scripts/Level Editor/Visual Scripting/TriggerColoranimation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerColoranimation : MonoBehaviour {
+
+	public void triggerColoranimation(EntityController ec) {
+        ec.playColoringAnimation();
+    }
+}
