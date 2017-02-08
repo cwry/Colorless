7f48cc67a7160c81a2677a187d2b59ccb26e686e
cwry <daniel.pochert@gmail.com>
2017-02-08 12:07:07 +0100

    intro

---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFade : MonoBehaviour {
+    public TextMesh txt;
+    public float speed;
+    bool animating;
+
+    public void animate() {
+        animating = true;
+        txt.color = new Color(1, 1, 1, 0);
+    }
+
+    void Awake() {
+        txt.color = new Color(1, 1, 1, 0);
+    }
+
+    void Update() {
+        if (animating) {
+            float newAlpha = txt.color.a + speed * Time.deltaTime;
+            if(newAlpha >= 1) {
+                animating = false;
+                newAlpha = 1;
+            }
+            txt.color = new Color(1, 1, 1, newAlpha);
+        }
+    }
+}
